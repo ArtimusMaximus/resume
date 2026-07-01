7926b4499fada4c5b32bb7058812efec96aa0b03
artimusmaximus <arthurmodymaniv@gmail.com>
2026-06-30 19:27:56 -0700

    updated portfolio with bachelors link, back-end developer certification, current resume

--- arthurportfolio/certs.js
+++ arthurportfolio/certs.js
@@ -149,8 +149,9 @@ const certz2 = [
 ];
 
 const certz3 = [
-    "aws-certified-cloud-practitioner_600_600.png",
+    "WGU_back_end_dev_cert.png",
     "WGU_front_end_dev_cert.png",
+    "aws-certified-cloud-practitioner_600_600.png",
     "Project+-png.png",
     "Meta_front_end_developer.avif",
     "google-it-support-professional-certificate-v2.png",
